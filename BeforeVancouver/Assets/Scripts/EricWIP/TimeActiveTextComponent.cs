0eda43b9fc7937c39e3a7b24a01665808fd01ae6
EricBoise <boiseeric@gmail.com>
2018-09-24 20:31:36 -0700

    YearData in FPS prefab

---
+++ BeforeVancouver/Assets/Scripts/EricWIP/TimeActiveTextComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeActiveTextComponent : MonoBehaviour
+{
+    public TextMeshPro textMesh;
+
+    private void OnEnable()
+    {
+        Missive.AddListener<YearDataMissive>(OnYearData);
+    }
+
+    private void OnDisable()
+    {
+        Missive.RemoveListener<YearDataMissive>(OnYearData);
+    }
+
+    private void OnYearData(YearDataMissive missive)
+    {
+        textMesh.text = missive.data.year.ToString();
+    }
+}
