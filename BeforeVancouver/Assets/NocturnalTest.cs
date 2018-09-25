5ff56b128a7028330eb2bfac26fb85255239db44
tedbarnett <mail@tedbarnett.com>
2018-09-25 13:59:10 -0700

    Wolves only at night, fixed Rift teleport problem (was falling through floor).

---
+++ BeforeVancouver/Assets/NocturnalTest.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NocturnalTest : MonoBehaviour
+{
+    bool isDayLast;
+    bool isDayNow;
+    public bool visibleInDay = true;
+    public bool visibleInNight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDayNow = GameObject.Find("ControlManager").GetComponent<ControlManager>().isDay;
+        isDayLast = isDayNow;
+        if (isDayNow && visibleInDay)
+        {
+            Debug.Log("Daytime: show objects");
+            showChildren(true);
+        }
+        else
+        {
+            Debug.Log("Daytime: hide objects");
+            showChildren(false);
+        }
+        if (!isDayNow && visibleInNight)
+        {
+            Debug.Log("Night: show objects");
+            showChildren(true);
+        }
+        else
+        {
+            Debug.Log("Night: hide objects");
+            showChildren(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isDayNow = GameObject.Find("ControlManager").GetComponent<ControlManager>().isDay;
+        if (isDayNow != isDayLast)
+        {
+            isDayLast = isDayNow;
+            if(isDayNow && visibleInDay)
+            {
+                Debug.Log("Daytime: show objects");
+                showChildren(true);
+            }
+            else
+            {
+                Debug.Log("Daytime: hide objects");
+                showChildren(false);
+            }
+            if (!isDayNow && visibleInNight)
+            {
+                Debug.Log("Night: show objects");
+                showChildren(true);
+            }
+            else
+            {
+                Debug.Log("Night: hide objects");
+                showChildren(false);
+            }
+        }
+    }
+    void showChildren(bool isVisible)
+    {
+        for (int a = 0; a < transform.childCount; a++)
+        {
+            transform.GetChild(a).gameObject.SetActive(isVisible);
+        }
+    }
+}
